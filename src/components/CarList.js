4c58c01c695669893cff5333a4dbc707d9360a96
Cuciuc Maxim <maxim.cuciuc123@gmail.com>
2025-09-03 11:09:12 +0300

    Am adaugat search bar dupa numele masinii

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import CarCard from "./CarCard";
 import cars from "../data/cars";
+import SearchBar from "./SearchBar";
 import "./CarList.css";
 
 function CarList() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredCars = cars.filter((car) =>
+    car.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="car-grid">
-      {cars.map((car) => (
-        <CarCard
-          key={car.id}
-          name={car.name}
-          year={car.year}
-          price={car.price}
-          image={car.image}
-          details={car.details}
-        />
-      ))}
+    <div>
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+      <div className="car-grid">
+        {filteredCars.map((car) => (
+          <CarCard
+            key={car.id}
+            name={car.name}
+            year={car.year}
+            price={car.price}
+            image={car.image}
+            details={car.details}
+          />
+        ))}
+      </div>
     </div>
   );
 }
